3e7938102e0b2d26be026f5d2e5b30e53a6eece8
Hunter Sutter <hjsutter@gmail.com>
2021-09-14 17:40:12 -0500

    evaluate function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,45 +1,50 @@
 package com.company;
 import java.io.FileNotFoundException;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 import java.io.File;
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         //Read in file name as input
-        Map<String, String> values = null;
+        Map<String, String> values = new HashMap<>();
         Scanner s = new Scanner(System.in);
         String filename = s.next();
         //Scan each line of the file one by one
         File file = new File(filename);
         Scanner f = new Scanner(file);
-        evaluate(f, values);
-        
-
+        String line = f.nextLine();
+        while(f.hasNextLine()){
+            Scanner l = new Scanner(line);
+            evaluate(l, values);
+            line = f.nextLine();
+        }
     }
 
     public static String evaluate(Scanner s, Map<String, String> vals){
-        Stack<String> stack = new Stack<String>();
-        String line = s.nextLine();
+        Stack<String> stack = new Stack<>();
         //Read each token in the line and add it to the stack
-        while(line != null) {
-            Scanner l = new Scanner(line);
-            String output = l.next();
-            while (l.hasNext()) {
-                stack.push(output);
-                output = l.next();
-            }
+        while(s.hasNext()) {
+            String output = s.next();
             stack.push(output);
-            System.out.println(stack.size());
-            System.out.println(stack.pop());
-            try{
-                line = s.nextLine();
-            } catch (NoSuchElementException n){
-                break;
+            output = s.next();
+            stack.push(output);
+        }
+        //If stack size is > 3 pop 3 items, else pop 2
+        if(stack.size() == 2){
+            String value = stack.pop();
+            if(value.length() > 2019){
+                value = "undefined";
             }
+            String varName = stack.pop();
+            vals.put(varName, value);
+//            if(vals.get(varName) != null) {
+                System.out.println(vals.get(varName));
+//            }
+        } else {
+            String param2 = stack.pop();
+            String operator = stack.pop();
+            String param1 = stack.pop();
         }
         return "";
     }
