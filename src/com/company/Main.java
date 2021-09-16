548af8896de68fac364e792d3623f76143392c9c
Hunter Sutter <hjsutter@gmail.com>
2021-09-16 12:53:21 -0500

    moved undefined check to operator methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,7 +20,7 @@ public class Main {
             evaluate(l, values);
             line = f.nextLine();
         }
-        for(Map.Entry<String, String> entry: values.entrySet()){
+        for (Map.Entry<String, String> entry : values.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
             System.out.print(key + " = ");
@@ -28,7 +28,7 @@ public class Main {
         }
     }
 
-    public static String evaluate(Scanner s, Map<String, String> vals) {
+    public static void evaluate(Scanner s, Map<String, String> vals) {
         Stack<String> stack = new Stack<>();
         //Read each token in the line and add it to the stack
         while (s.hasNext()) {
@@ -41,6 +41,8 @@ public class Main {
         while (stack.size() > 2) {
             String result = "";
             String param2 = stack.pop();
+            String operator = stack.pop();
+            String param1 = stack.pop();
             //If parameter is a variable check the map to see if it has an assignment, otherwise set to undefined
             if (Character.isLetter(param2.charAt(0))) {
                 param2 = vals.get(param2);
@@ -48,19 +50,12 @@ public class Main {
                     param2 = "undefined";
                 }
             }
-            String operator = stack.pop();
-            String param1 = stack.pop();
             //If parameter is a variable check the map to see if it has an assignment, otherwise set to undefined
             if (Character.isLetter(param1.charAt(0))) {
                 param1 = vals.get(param1);
                 if (param1 == null) {
                     param1 = "undefined";
                 }
-            }
-            //If either parameter is undefined evaluate the operation as undefined and continue
-            if (param1.equals("undefined") || param2.equals("undefined")) {
-                stack.push("undefined");
-                break;
             }
             //Calls the appropriate method depending on the operator
             switch (operator) {
@@ -76,56 +71,65 @@ public class Main {
             }
             //Pushes the result of the operation on the stack and continues to the next operation
             stack.push(result);
+            //At this point all operations should be done and the variable should be stored in the map
+            String value = stack.pop();
+            if (value.length() > 2019) {
+                value = "undefined";
+            }
+            String varName = stack.pop();
+            vals.put(varName, value);
         }
-        //At this point all operations should be done and the variable should be stored in the map
-        String value = stack.pop();
-        if (value.length() > 2019) {
-            value = "undefined";
-        }
-        String varName = stack.pop();
-        vals.put(varName, value);
-        return value;
     }
 
     public static String concatenate(String x, String y) {
-        return x + y;
+        if(x.equals("undefined") || y.equals("undefined")){
+            return "undefined";
+        } else {
+            return x + y;
+        }
     }
 
     public static String interleave(String x, String y) {
         int count;
         char[] charsX = x.toCharArray();
         char[] charsY = y.toCharArray();
-        String result = "";
+        StringBuilder result = new StringBuilder();
         if (charsX.length == charsY.length) {
             for (int i = 0; i < charsX.length; i++) {
-                result = result + charsX[i];
-                result = result + charsY[i];
+                result.append(charsX[i]);
+                result.append(charsY[i]);
             }
         } else if (charsX.length < charsY.length) {
             count = 0;
             for (int i = 0; i < charsX.length; i++) {
-                result = result + charsX[i];
-                result = result + charsY[i];
+                result.append(charsX[i]);
+                result.append(charsY[i]);
                 count++;
             }
             for (int i = count; i < charsY.length; i++) {
-                result = result + charsY[i];
+                result.append(charsY[i]);
             }
         } else {
             count = 0;
             for (int i = 0; i < charsY.length; i++) {
-                result = result + charsX[i];
-                result = result + charsY[i];
+                result.append(charsX[i]);
+                result.append(charsY[i]);
                 count++;
             }
             for (int i = count; i < charsX.length; i++) {
-                result = result + charsX[i];
+                result.append(charsX[i]);
             }
         }
-        return result;
+        if(x.equals("undefined") || y.equals("undefined")){
+            result.replace(0, result.length(), "undefined");
+        }
+        return result.toString();
     }
 
     public static String splice(String x, String y) {
+        if(x.equals("undefined") || y.equals("undefined")){
+            return "undefined";
+        }
         int mid = y.length() / 2;
         String[] split = new String[2];
         split[0] = y.substring(0, mid);
